28feab43dacebb917bf2f1ee8c3a7db8900646c5
Pascal Vree <pvree@mirabeau.nl>
2016-03-16 17:48:48 +0100

    Baptized Application with a proper name; fixed a minor bug, the application now waits for the threads to finish before exiting ...

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,16 +7,16 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var slackEndpoint = new Uri("");
+            var slackEndpoint = new Uri("https://hooks.slack.com/services/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");
 
-            const string endpointSonarQubeMeasurements = "";
-            const string metrics = "";
+            const string endpointSonarQubeMeasurements = "http://zow-dev1:9000/api/resources?depth=0";
+            const string metrics = "metrics=ncloc,public_api,duplicated_blocks,complexity,public_documented_api_density,statements,functions,files,classes,violations";
 
             var endpointZowonenBusinessSonarQubeMeasurements = new Uri($"{endpointSonarQubeMeasurements}&resource=3&{metrics}");
             var endpointZowonenWebSonarQubeMeasurements = new Uri($"{endpointSonarQubeMeasurements}&resource=5&{metrics}");
 
-            UploadSonarMeasurementsNaarSlackTask.RunAsync(endpointZowonenBusinessSonarQubeMeasurements, slackEndpoint);
-            UploadSonarMeasurementsNaarSlackTask.RunAsync(endpointZowonenWebSonarQubeMeasurements, slackEndpoint);
+            UploadSonarMeasurementsNaarSlackTask.RunAsync(endpointZowonenBusinessSonarQubeMeasurements, slackEndpoint).Wait();
+            UploadSonarMeasurementsNaarSlackTask.RunAsync(endpointZowonenWebSonarQubeMeasurements, slackEndpoint).Wait();
         }
     }
 }
--- ConsoleApplication1/Tasks/UploadSonarMeasurementsNaarSlackTask.cs
+++ ConsoleApplication1/Tasks/UploadSonarMeasurementsNaarSlackTask.cs
@@ -10,6 +10,29 @@ using ConsoleApplication1.Models;
 
 namespace ConsoleApplication1.Tasks
 {
+    /* van:
+    [{
+        "id": 3,
+        "key": "ZOWonen:ZOWonen:2487B82B-554F-40F7-826B-3F0D2B1A5229",
+        "name": "ZoWonen.Business",
+        "scope": "PRJ",
+        "qualifier": "BRC",
+        "date": "2016-03-15T15:53:14+0100",
+        "creationDate": "2015-10-08T14:07:39+0200",
+        "lname": "ZoWonen.Business",
+        "version": "0.1",
+        "branch": "2487B82B-554F-40F7-826B-3F0D2B1A5229",
+        "description": "",
+        "msr": [{
+            "key": "ncloc",
+            "val": 3667.0,
+            "frmt_val": "3,667"
+        }]
+    }]
+
+    naar: { text: "{"project":"ZoWonen.Business","creationDate":"2015-10-08T14:07:39+0200","ncloc":"3,667","public_api":"769","duplicated_blocks":"4","complexity":"216","public_documented_api_density":"13.3%","statements":"568","functions":"128","files":"188","classes":"169","violations":"625"}" }
+    */
+
     public static class UploadSonarMeasurementsNaarSlackTask
     { 
         public static async Task RunAsync(Uri endpointSonarQubeMeasurements, Uri slackEndpoint)
@@ -19,15 +42,14 @@ namespace ConsoleApplication1.Tasks
 
             using (var client = new HttpClient())
             {
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                var response = await client.GetAsync(endpointSonarQubeMeasurements);
-                var responseBody = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(responseBody);
-
                 try
                 {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var response = await client.GetAsync(endpointSonarQubeMeasurements);
+                    var responseBody = await response.Content.ReadAsStringAsync();
+
                     var serializer = new JavaScriptSerializer();
                     var sonarQubeMetrics = serializer.Deserialize<SonarQubeMetricModel[]>(responseBody).ToList();
 
